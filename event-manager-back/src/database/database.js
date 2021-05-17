53ae8d2dfc1dbd8fd3ebe71a2deccc7090e46e6e
Debora <deborarebelatto1@gmail.com>
2021-05-17 14:50:50 -0300

    17/05

--- event-manager-back/src/database/database.js
+++ event-manager-back/src/database/database.js
@@ -2,18 +2,9 @@ const mongoose = require("mongoose");
 
 // Conectando no Mongo Atlas
 mongoose.connect(
-   'mongodb+srv://user1:passwordeventmanger123@cluster0.u7qw2.mongodb.net/managerdb?retryWrites=true&w=majority',
+  process.env.MONGO_ATLAS,
   { useNewUrlParser: true, useUnifiedTopology: true }
 )
-  // process.env.MONGO_ATLAS,
-  // { useNewUrlParser: true, useUnifiedTopology: true }
-// )
-
-// Conectando no MongoDB Local
-// mongoose.connect(
-//   'mongodb://localhost:27017/event_manager',
-//   { useNewUrlParser: true, useUnifiedTopology: true }
-// )
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
